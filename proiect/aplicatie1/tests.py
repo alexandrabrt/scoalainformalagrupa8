fd9ef25ac23bf2ec351ea7915b3b6107c37770b9
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2022-05-09 22:03:02 +0300

    selenium test

--- proiect/aplicatie1/tests.py
+++ proiect/aplicatie1/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import datetime
+import unittest
 
-# Create your tests here.
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class LocationTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get('http://127.0.0.1:8000')
+        username = self.driver.find_element_by_name('username')
+        username.send_keys('alexandra')
+        password = self.driver.find_element_by_name('password')
+        password.send_keys('Init@123')
+        submit = self.driver.find_element_by_class_name('btn-info')
+        submit.submit()
+        self.logs = open('logs.txt', mode='a')
+
+    def test_form(self):
+        if value := 'table' in self.driver.page_source:
+            self.logs.write(f"{value}, {datetime.datetime.now()}\n")
+        assert value
+
+    # def test_add_location(self):
+
